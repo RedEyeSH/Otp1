7694d677f95ac0c4b88a1d25e5a51301d3513235
RedEyeSH <97534002+RedEyeSH@users.noreply.github.com>
2025-09-06 13:43:50 +0300

    Add Account class with deposit and withdrawal functionality and corresponding tests

--- src/main/java/week3/HomeAssignment/Account.java
+++ src/main/java/week3/HomeAssignment/Account.java
@@ -1,4 +1,30 @@
 package week3.HomeAssignment;
 
 public class Account {
+    private double balance;
+
+    public Account() {
+        this.balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("The amount given must be greater than 0");
+        }
+    }
+
+    public double withdraw(double amount) {
+        if (amount <= balance && amount > 0) {
+            balance -= amount;
+            return amount;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
--- src/test/java/week3/HomeAssignment/AccountTest.java
+++ src/test/java/week3/HomeAssignment/AccountTest.java
@@ -1,4 +1,47 @@
+package week3.HomeAssignment;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class AccountTest {
-  
+    Account acc = new Account();
+
+    @Test
+    void testDeposit() {
+        acc.deposit(100.0);
+        assertEquals(100.0, acc.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        acc.deposit(50.0);
+        assertEquals(20.0, acc.withdraw(20));
+    }
+
+    @Test
+    void testCheckBalance() {
+        assertEquals(0, acc.getBalance());
+    }
+
+    @Test
+    void testDepositNegativeAmount() {
+        acc.deposit(-50.0);
+        assertEquals(0.0, acc.getBalance());
+    }
+
+    @Test
+    void testWithdrawNegativeAmount() {
+        acc.deposit(100.0);
+        assertEquals(0, acc.withdraw(-30.0));
+        assertEquals(100.0, acc.getBalance());
+    }
+
+    @Test
+    void testWithdrawOverBalance() {
+        acc.deposit(100.0);
+        assertEquals(0.0, acc.withdraw(150.0));
+        assertEquals(100.0, acc.getBalance());
+    }
 }
+
+// Opiskelija: Quang (Than Ngoc)
